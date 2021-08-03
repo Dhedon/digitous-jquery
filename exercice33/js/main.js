a3d742410fa6f22fd2a8c5c967b39e4067ca7418
Céline <dhedontsering@yahoo.fr>
2021-08-03 14:47:41 +0200

    finish with all my Exercices of jquery

---
+++ exercice33/js/main.js
@@ -0,0 +1,11 @@
+$(".btn").click(function(){
+    $.ajax({
+        url: 'https://restcountries.eu/rest/v2/name/france',
+        success: function(data, statuts, response) {
+            
+            $('#country').html(data[0].name);
+            $("#capital").html(data[0].capital);
+        }
+    });
+})
+
